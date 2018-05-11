fea3903f5e925c2c000a54275375fc1c14377a68
Vytis Vadoklis <vytis.vadoklis@outlook.com>
2018-05-11 13:13:18 +0100

    post scaffold launched, users not connected with posts

--- db/migrate/20180511114906_create_posts.rb
+++ db/migrate/20180511114906_create_posts.rb
@@ -1,7 +1,8 @@
 class CreatePosts < ActiveRecord::Migration[5.1]
   def change
     create_table :posts do |t|
-      t.string :message
+      t.text :content
+      t.integer :user_id
 
       t.timestamps
     end
