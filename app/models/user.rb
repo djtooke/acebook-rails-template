33af5d002e013751f2ad1a2198b32e5e9f94676d
Vytis Vadoklis <vytis.vadoklis@outlook.com>
2018-05-11 15:22:36 +0100

    users and posts connected

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  has_many :microposts
-  validates :name, presence:true,
-  length: {minimum: 6}
-validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
+  has_many :posts
+#   validates :name, presence:true,
+#   length: {minimum: 6}
+# validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
 
 end
