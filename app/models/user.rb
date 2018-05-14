c17731829c35d1c4b9b7a7474893718b7e352523
Vytis Vadoklis <vytis.vadoklis@outlook.com>
2018-05-14 17:28:11 +0100

    user scaff, show user id and name on post

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
+  has_many :posts
+  validates :name, presence:true,
+  length: {minimum: 6}
+validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
 end
