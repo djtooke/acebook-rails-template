903e0dee4c9cdf396a577ced2d2e7ce0708cdf90
Vytis Vadoklis <vytis.vadoklis@outlook.com>
2018-05-16 17:00:09 +0100

    user login and sign up devise setup (routing not working properly)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  get '/',  to: 'posts#index'
+  
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # root 'users#post'
   resources :posts
   resources :users
 end
+
