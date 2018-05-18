c647be18d50098fa7e4e07803d8dd9f7cbb4c9fc
Jack Dewinter <jackdewinter1@gmail.com>
2018-05-18 14:44:23 +0100

    more tests

--- app/controllers/home_controller.rb
+++
@@ -1,8 +0,0 @@
-class HomeController < ApplicationController
-  before_action :authenticate_user!
-  def index
-    puts user_signed_in?
-    puts current_user.email
-    puts user_session
-  end  
-end
